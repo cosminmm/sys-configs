afadda361a18c8dabbf8772914e671fad6eccb70
cosminmm <cosmin.marius@gmail.com>
2026-01-14 18:03:26 +0200

    Add files via upload

---
+++ nvim/lua/custom/plugins/svelte-js-html.lua
@@ -0,0 +1,14 @@
+-- INFO:
+-- In Svelte projects you have to install:
+-- npm install --save-dev prettier prettier-plugin-svelte
+-- add .prettierrc file in root project folder with the following content:
+-- {
+--   "plugins": ["prettier-plugin-svelte"]
+-- }
+
+return {
+  'pangloss/vim-javascript',
+  'othree/html5.vim',
+  'evanleck/vim-svelte',
+  'leafOfTree/vim-svelte-plugin',
+}
---
+++ nvim/lua/custom/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false, -- important: să fie disponibil imediat
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<CR>", desc = "Tmux left" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<CR>", desc = "Tmux down" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<CR>", desc = "Tmux up" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<CR>", desc = "Tmux right" },
+    },
+  },
+}
